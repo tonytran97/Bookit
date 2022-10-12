de56991943a6835d50200b770e816ca029a784cc
slendyG <josuegarza0608@gmail.com>
2022-10-11 20:59:41 -0700

    Add index.js file for models

--- Models/Index.js
+++ Models/Index.js
@@ -1,3 +1,23 @@
 const User = require('./Users');
 const Review = require('./Review');
 const Book = require('./Book');
+
+User.hasMany(Review, {
+    foreignKey:  'user_id',
+    onDelete: 'CASCADE'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Book, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Book.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Review, Book }
