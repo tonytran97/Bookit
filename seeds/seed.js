e2fa146664a180fba36ab4e6c27ecf9a3b988d17
Akarim Abdi <108248869+K2YFam@users.noreply.github.com>
2022-10-11 19:13:23 -0700

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,9 @@
 const sequelize = require('../config/connection');
 const { User, Book, Review } = require('../model');
 
-const userData = require('./userData.json');
-const bookData = require('./bookData.json');
-const reviewData = require('./reviewData.json');
+const userData = require('./userData');
+const bookData = require('./bookData');
+const reviewData = require('./reviewData');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
