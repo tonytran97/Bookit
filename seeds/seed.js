435bd0091b9bdd59b7c73f58376a7b3b74dbaf9e
Akarim Abdi <108248869+K2YFam@users.noreply.github.com>
2022-10-11 19:12:01 -0700

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Book, Categories } = require('../model');
+const { User, Book, Review } = require('../model');
 
 const userData = require('./userData.json');
 const bookData = require('./bookData.json');
-const categoriesData = require('./categoriesData.json');
+const reviewData = require('./reviewData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,7 +12,7 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  await Categories.bulkCreate(categoriesData);
+  await Review.bulkCreate(reviewData);
 
   await Book.bulkCreate(bookData);
 
