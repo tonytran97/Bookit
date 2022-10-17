c40e1e6f532bc7952f7eb3459b20be7125a10dfe
Akarim Abdi <108248869+K2YFam@users.noreply.github.com>
2022-10-17 00:26:16 -0700

    User Json/ updates

---
+++ Models/Book.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ISBN: {
+      type: DataTypes.STRING,
+    },
+    VOTES: {
+      type: DataTypes.INTEGERm
+    }
+      },
+
+{
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'book',
+    },
+);
+
+
+module.exports = Book;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,15 @@
 const sequelize = require('../config/connection');
+<<<<<<< Updated upstream
 const { User, Book, Review } = require('../model');
 
 const userData = require('./userData');
 const bookData = require('./bookData');
 const reviewData = require('./reviewData');
+=======
+const { User } = require('../models');
+
+const userData = require('./userData.json');
+>>>>>>> Stashed changes
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,11 +18,18 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+<<<<<<< Updated upstream
   await Review.bulkCreate(reviewData);
 
   await Book.bulkCreate(bookData);
+=======
+>>>>>>> Stashed changes
 
   process.exit(0);
 };
 
+<<<<<<< Updated upstream
 seedDatabase();
+=======
+seedDatabase();
+>>>>>>> Stashed changes
